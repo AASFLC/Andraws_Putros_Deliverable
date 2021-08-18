3a89fffa02e776c7983d8a7519973363ae141a1e
AASFLC <53628253+AASFLC@users.noreply.github.com>
2021-08-17 23:22:08 -0400

    Add files via upload

---
+++ src/game/Main.java
@@ -0,0 +1,11 @@
+package game;
+
+/**
+ * Andraws Putros
+ * Student ID: 991407087
+ */
+public class Main {
+    public static void main(String[] args) {
+        RunTheGame.runTheGame();
+    }
+}
